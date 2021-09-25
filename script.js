f2a4033bee546ab70c996a32bf94789b1a494c52
Francisco Raigoza <raigoza.frankie@gmail.com>
2021-09-25 00:33:16 -0700

    styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const pokedex = document.getElementById('pokedex')
-console.log(pokedex)
+const pokedex = document.getElementById('pokedex');
+console.log(pokedex);
 
 const fetchPokemon = () => {
   const promises = [];
   for (let i = 1; i < 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) =>  res.json()));
+    promises.push(fetch(url).then((res) => res.json()));
   }
   Promise.all(promises).then((results) => {
     const pokemon = results.map((data) => ({
@@ -14,17 +14,21 @@ const fetchPokemon = () => {
       image: data.sprites['front_default'],
       type: data.types.map((type) => type.type.name).join(', '),
     }));
-    showPokemon(pokemon)
+    showPokemon(pokemon);
   });
 };
 const showPokemon = (pokemon) => {
-  const pokemonString = pokemon.map(pokeChar => `
+  const pokemonString = pokemon
+    .map(
+      (pokeChar) => `
     <li class="card"> 
       <img class="card-img" src=${pokeChar.image} alt="" />
       <h2 class="card-heading">${pokeChar.id}. ${pokeChar.name}</h2>
       <p class="card-subheading">Type: ${pokeChar.type}</p>
     </li>
-  `).join('')
-  pokedex.innerHTML = pokemonString
-}
+  `
+    )
+    .join('');
+  pokedex.innerHTML = pokemonString;
+};
 fetchPokemon();
